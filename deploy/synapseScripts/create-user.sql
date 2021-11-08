2ef46460293953f20396932974d419632c450399
Frederik Gheysels <frederik.gheysels@telenet.be>
2021-11-08 18:06:03 +0100

    Improve create-user sql script

--- deploy/synapseScripts/create-user.sql
+++ deploy/synapseScripts/create-user.sql
@@ -1,17 +1,18 @@
--- The below is no longer necessary
--- USE MASTER
--- CREATE LOGIN [frederik.gheysels@codit.eu] FROM EXTERNAL PROVIDER;
-
--- USE climatebox_rawdata
---CREATE USER [frederik.gheysels@codit.eu] FROM EXTERNAL PROVIDER;
-
 -- Create a contained user in Synapse for an Azure Function
 -- https://docs.microsoft.com/en-us/azure/azure-sql/database/authentication-aad-configure?tabs=azure-powershell
-USE climatebox_rawdata
+-- The username has the same name as the Azure FunctionApp.
+USE db1
 CREATE USER [fg-datalakepoc-rawdataprocessor] FROM EXTERNAL PROVIDER;
 
 ALTER ROLE db_datareader ADD MEMBER [fg-datalakepoc-rawdataprocessor];
 
 GRANT ADMINISTER DATABASE BULK OPERATIONS TO [fg-datalakepoc-rawdataprocessor]
 
--- Connect to the Synapse DB via this connectionstring:
+
+-- Execute the below commands if you want to be able to login to Synapse with
+-- your own credentials.  (The e-mail address must be known in Azure AD)
+-- USE MASTER
+-- CREATE LOGIN [your-emailaddress] FROM EXTERNAL PROVIDER;
+
+-- USE db1
+--CREATE USER [your-emailaddress] FROM EXTERNAL PROVIDER;
